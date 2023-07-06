6f597d368e2623fa6f6b4d935f3e8e7e5a03daea
Jun Zhang <bigonez@gmail.com>
2023-07-06 19:26:01 +1000

    Implement fetch data from database.
    . implement to fetch data from database, regressor.js.

--- regressor.js
+++ regressor.js
@@ -2,15 +2,33 @@ const sqlite3 = require('better-sqlite3');
 const utaDb = new sqlite3('./database/uta100_optimal.db3', {fileMustExist: true});
 
 const queryRegressor = function (finishTime, referSet) {
-  range = [0, 834]
-  eppData = [1., 2., 3., 4., 11., 12., 13., 14., 21., 22., 23., 24., 31., 32., 33., 34., 41., 42.]
+	// form the query
+	var optimalQuery = "SELECT location, AVG(proportion) AS mean, lpid, upid FROM uta100_final_proportion"
+	if( referSet ) {
+		optimalQuery += " LEFT JOIN (SELECT MIN(id) AS lpid, MAX(id) AS upid FROM ( " +
+			"SELECT id, racestamp, ABS(racestamp - "+(finishTime * 3600)+") AS rsdiff FROM uta100_athlete " +
+			"WHERE status=1 ORDER BY rsdiff LIMIT "+ referSet +")) WHERE pid >= lpid AND pid <= upid"
+	}
+	else {
+		optimalQuery += " LEFT JOIN (SELECT MIN(id) AS lpid, MAX(id) AS upid FROM uta100_athlete WHERE status=1)"
+	}
+	optimalQuery += " GROUP BY location ORDER BY location"
 
-  return {
-       range : range,
-         epp : eppData
-  }
+	// prepare the query statement
+	const optimalSTMT = utaDb.prepare(optimalQuery);
+
+	// get the regressor data
+	const row = optimalSTMT.get()
+	const range = [row.lpid, row.upid]
+	const eppData = optimalSTMT.all().map(row => Math.exp(Math.PI - row.mean));
+
+	// return the query result
+	return {
+		range : range,
+		epp   : eppData
+	}
 }
 
 module.exports = {
-  queryRegressor
+	queryRegressor
 }
