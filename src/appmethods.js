bc18d8b65e1276ebadbc09ca46f23e76d03c8227
Jun Zhang <bigonez@gmail.com>
2025-06-01 21:34:01 +1000

    Update by UTA 2025 race result.
    . update by UTA 2025 race result.
      . update the database name as uta_nodes.db3, .env.production;
      . update the API interface to accept the new parameter, event, appmethods.js;
      . support the multiple races, UTA Miler & UTA100, racenodes.js;
      . update the packages to the latest version, package.json & pnpm-lock.yaml;
      . update the project introduction, README.md.

--- src/appmethods.js
+++ src/appmethods.js
@@ -1,14 +1,14 @@
 const appRoot = (req, res) => {
     res.json({
-        "Title": 'Nodes API for UTA100 Planner',
-        "Usage": 'https://utanodes.vercel.app/nodes?finishtime=[expected finish time]&reference=[dataset size]',
-        "Example":'https://utanodes.vercel.app/nodes?finishtime=20&reference=100'
+        "Title": 'Nodes API for UTA Planner',
+        "Usage": 'https://utanodes.vercel.app/nodes?event=[race]&finishtime=[expected finish time]&reference=[dataset size]',
+        "Example":'https://utanodes.vercel.app/nodes?event=1&finishtime=20&reference=100'
     });
 };
 
 const appAbout = (req, res) => {
     res.json({
-        "About": "Nodes API for UTA100 Planner",
+        "About": "Nodes API for UTA Planner",
         "Environment": process.env.NODE_ENV,
         "Service Port": process.env.RACENODES_APP_PORT,
         "Working Path": process.cwd(),
