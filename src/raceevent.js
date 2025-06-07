0df66f558ecb727c14a2486bae319c8e1c8fce2a
Jun Zhang <bigonez@gmail.com>
2025-06-07 15:36:23 +1000

    Export event profile & configuration.
    . export the event's profile & configuration.
      . add the event's profile & config data, raceconfig.js;
      . simplify the course config data, then combine all data together to export, raceevent.js;
      . update the database, uta_nodes.db3.

--- src/raceevent.js
+++ src/raceevent.js
@@ -1,15 +1,27 @@
 import sqlite3 from 'better-sqlite3';
 
+import { eventProfiles, eventsConfig } from './raceconfig.js';
+
 // form the database name
 const dbName = process.cwd() + '/src/uta_nodes.db3';
 // initial the database connection
 const raceDb = new sqlite3(dbName, {fileMustExist: true});
 
 const queryEvent = (raceDb, event) => {
+    let eventCPNos = [];
+    let eventCPNames = [];
+    let eventCPOdos = [];
+    let eventCutOffStrs = [];
+
     // prepare the query statement
     const tpSTMT = raceDb.prepare("SELECT location, abbr, alias, odometer, IFNULL(cutoff, '') AS cutoff FROM uta_location WHERE event = :event AND alias NOT NULL ORDER BY location");
-    // get the all timing locations' data
-    const eventTimePoints = tpSTMT.all({event: event});
+    // get the all valid check points' data
+    for (const location of tpSTMT.iterate({event: event})) {
+        eventCPNos.push(location.abbr);
+        eventCPNames.push(location.alias);
+        eventCPOdos.push(location.odometer);
+        eventCutOffStrs.push(location.cutoff);
+    }
 
     // prepare the query statement
     const ignoreSTMT = raceDb.prepare("SELECT location FROM uta_location WHERE event = :event AND alias IS NULL ORDER BY location");
@@ -21,16 +33,20 @@ const queryEvent = (raceDb, event) => {
 
     // return the query result
     return {
-        event : event,
-        timePoints : eventTimePoints,
-        ignoreIdxs : eventIgnores
+              cpNos : eventCPNos,
+            cpNames : eventCPNames,
+             cpOdos : eventCPOdos,
+         cutOffStrs : eventCutOffStrs,
+        arrivalIdxs : eventIgnores
     }
 }
 
 export default (req, res) => {
     const event = isNaN(parseInt(req.query.event)) ? 1 : parseInt(req.query.event);
 
-    res.json(
-        queryEvent(raceDb, event)
-    );
+    res.json({
+        eventProfile: eventProfiles[event],
+        eventConfig: eventsConfig[event],
+        courseConfig: queryEvent(raceDb, event)
+    });
 }
